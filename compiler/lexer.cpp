f7df2bcbb88298e48ce6db29382a23f474a96a4b
tareq <siemboy@163.com>
2019-07-16 10:25:35 +0800

    lexer try

---
+++ compiler/lexer.cpp
@@ -0,0 +1,62 @@
+#include"lexer.h"
+
+Scanner::Scanner(char* name){
+	file = fopen(name, "r");
+	if(!file){
+		cout<<file<<"open fail"<<endl;
+	}
+	fileName = name;
+	//init scanner state
+	lineLen = 0;
+	readPos = -1;
+	lastCh = 0;
+	lineNum = 1;
+	colNum = 0;
+}
+
+Scanner::~Scanner(){
+	if(file){
+		cout<<file<<"scan finish"<<endl;
+		fclose(file);
+	}
+}
+
+char* Scanner::scan(){
+	if(!file){
+		return -1;
+	}
+	else{
+		if(readPos == lineLen - 1){
+			lineLen = fread(line, 1, BUFFLEN, file);
+			if(lineLen == 0){
+				lineLen = 1;
+				line[0] = -1;
+			}
+			else{
+
+			}
+			readPos = -1;
+		}
+		readPos++;
+		char ch = line[readPos];
+		if(lastch == '\n'){
+			lineNum++;
+			colNum = 0;
+		}
+		else{
+
+		}
+		if(ch == -1){
+			fclose(file);
+			file = NULL;
+		}
+		else if(ch != '\n'){
+			colNum++;
+		}
+		else{
+			
+		}
+		lastCh = ch;
+		return ch;
+	}
+}
---
+++ compiler/lexer.h
@@ -0,0 +1,25 @@
+//scan file character by character
+class Scanner{
+	//file pointer
+	char* fileName;
+	FILE* file;
+	//internal state
+	static const int BUFFLEN = 80;
+	char line[BUFFLEN];
+	int readPos;
+	char lastch;
+	int lineLen;//actual read bytes
+	//read done state
+	int lineNum;
+	int colNum;
+
+public:
+	Scanner(char* name);
+	~Scanner();
+
+	char scan();
+
+	char* getFile();
+	int getLine();
+	int getCol();
+}
